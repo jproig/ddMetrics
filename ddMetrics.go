5fe64bc5a471edd9da295c9eedb149194a9b7a01
Juan Pablo Roig <juan.pablo.roig@gmail.com>
2020-12-28 11:25:15 -0800

    Ramdomize the filename

--- ddMetrics.go
+++ ddMetrics.go
@@ -1,26 +1,31 @@
 package main
 
 import (
+	"context"
+	"encoding/hex"
 	"flag"
+	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
 	"os"
 	"os/exec"
-	"fmt"
+	"path/filepath"
 	"regexp"
 	"strconv"
-	"context"
 	"time"
 
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
+
 const (
 	cmdTimeout = 2
 )
+
 var (
 	appVersion string
-	version = prometheus.NewGauge(prometheus.GaugeOpts{
+	version    = prometheus.NewGauge(prometheus.GaugeOpts{
 		Name: "version",
 		Help: "Version information about this binary",
 		ConstLabels: map[string]string{
@@ -39,21 +44,28 @@ var (
 	}, []string{"bs", "count"})
 
 	ddWriteThroughput = prometheus.NewGaugeVec(prometheus.GaugeOpts{
-                Name: "dd_writes_throughput_MBs_v2",
-                Help: "Throughput of all dd writes",
-        }, []string{"bs", "count"})
+		Name: "dd_writes_throughput_MBs_v2",
+		Help: "Throughput of all dd writes",
+	}, []string{"bs", "count"})
 )
 
-func runDD(bs,count,destDir string) {
+// tempFileName generates a temporary filename for use in testing or whatever
+func tempFileName(dir, prefix, suffix string) string {
+	randBytes := make([]byte, 16)
+	rand.Read(randBytes)
+	return filepath.Join(dir, prefix+hex.EncodeToString(randBytes)+suffix)
+}
+
+//runDD executes the dd os command, parses the output and pushes the results to the metrics.
+func runDD(bs, count, destDir string) {
 	// TODO(jproig): improve strconv error handling
 	bsArg := fmt.Sprintf("bs=%s", bs)
 	countArg := fmt.Sprintf("count=%s", count)
-	ofArg := fmt.Sprintf("of=%s/test_file", destDir)
-	
+	ofArg := fmt.Sprintf("of=%s", tempFileName(destDir, "test_file_", ".tmp"))
 
 	ctx, cancel := context.WithTimeout(context.Background(), cmdTimeout*time.Minute)
 	defer cancel() // The cancel should be deferred so resources are cleaned up
-    cmd := exec.CommandContext(ctx, "dd", "if=/dev/zero", ofArg, bsArg, countArg, "conv=fsync")
+	cmd := exec.CommandContext(ctx, "dd", "if=/dev/zero", ofArg, bsArg, countArg, "conv=fsync")
 
 	if ctx.Err() == context.DeadlineExceeded {
 		log.Printf("Command timed out")
@@ -64,29 +76,29 @@ func runDD(bs,count,destDir string) {
 	out, err := cmd.CombinedOutput()
 	if err != nil {
 		ddWriteTotal.WithLabelValues(bs, count, "err").Inc()
-	    log.Printf("cmd.Run() failed with %s\n", err)
+		log.Printf("cmd.Run() failed with %s\n", err)
 		return
 	}
 
 	var throughputReMB = regexp.MustCompile(`, ([0-9]*\.[0-9]+|[0-9]+) MB/s`)
-    throughputMB := throughputReMB.FindStringSubmatch(string(out))
+	throughputMB := throughputReMB.FindStringSubmatch(string(out))
 	if len(throughputMB) > 0 {
-		th, _ := strconv.ParseFloat(throughputMB[1],64)
+		th, _ := strconv.ParseFloat(throughputMB[1], 64)
 		ddWriteThroughput.WithLabelValues(bs, count).Add(th)
-    }
-    if len(throughputMB) == 0 {
-      var throughputReGB = regexp.MustCompile(`, ([0-9]*\.[0-9]+|[0-9]+) GB/s`)
-      throughputGB := throughputReGB.FindStringSubmatch(string(out))
-      th, _ := strconv.ParseFloat(throughputGB[1],64)
-	  ddWriteThroughput.WithLabelValues(bs, count).Add(th*1000) 
-    }
+	}
+	if len(throughputMB) == 0 {
+		var throughputReGB = regexp.MustCompile(`, ([0-9]*\.[0-9]+|[0-9]+) GB/s`)
+		throughputGB := throughputReGB.FindStringSubmatch(string(out))
+		th, _ := strconv.ParseFloat(throughputGB[1], 64)
+		ddWriteThroughput.WithLabelValues(bs, count).Add(th * 1000)
+	}
 
 	var durationRe = regexp.MustCompile(`, ([0-9]*\.[0-9]+|[0-9]+) s,`)
-    duration := durationRe.FindStringSubmatch(string(out))
-    if len(duration) > 0 {
-        d, _ := strconv.ParseFloat(duration[1],64)
+	duration := durationRe.FindStringSubmatch(string(out))
+	if len(duration) > 0 {
+		d, _ := strconv.ParseFloat(duration[1], 64)
 		ddWriteDuration.WithLabelValues(bs, count).Add(d)
-    }
+	}
 
 	ddWriteTotal.WithLabelValues(bs, count, "ok").Inc()
 	log.Printf("Wrote bs=%s count %s", bs, count)
@@ -109,14 +121,14 @@ func main() {
 	r.MustRegister(ddWriteThroughput)
 	r.MustRegister(version)
 
-    go func(){
-    	time.Sleep(20*time.Second)
-    	for n:=1; n <= loopCount; n++ {
-		    runDD("1G","1",writeOnDir)
-		    runDD("64M","1",writeOnDir)
-		    runDD("1M","256",writeOnDir)
-		    runDD("8k","10k",writeOnDir)
-		    runDD("512","1000",writeOnDir)
+	go func() {
+		time.Sleep(20 * time.Second)
+		for n := 1; n <= loopCount; n++ {
+			runDD("1G", "1", writeOnDir)
+			runDD("64M", "1", writeOnDir)
+			runDD("1M", "256", writeOnDir)
+			runDD("8k", "10k", writeOnDir)
+			runDD("512", "1000", writeOnDir)
 		}
 		fmt.Printf("Done dding in the disk %s, will stay up so our metrics can be scraped.", writeOnDir)
 	}()
